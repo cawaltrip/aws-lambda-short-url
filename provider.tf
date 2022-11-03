f24fcb17e47d62256746b55c14e54bc1425111ad
Chris Waltrip <chris@walt.rip>
2022-11-03 11:30:45 -0700

    Added variable for AWS profile name

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 provider "aws" {
   region = var.region
+  profile = "skellies" # Could use `var.profile_name` here instead.
 }
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
+  profile = var.profile_name
   alias  = "cloudfront_acm"
 }
-
 provider "archive" {
 }
 
--- variables.tf
+++ variables.tf
@@ -1,9 +1,13 @@
 variable "region" {
   type        = string
-  default     = "eu-west-1"
+  default     = "us-west-2"
   description = "The AWS reegion to use for the Short URL project."
 }
 variable "short_url_domain" {
   type        = string
   description = "The domain name to use for short URLs."
+}
+variable "profile_name" {
+  type = string
+  description = "The AWS profile name to use from credentials file."
 }
