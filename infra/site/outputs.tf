e7c33e7699d5df50422a96f63062ae083970e901
Chris Waltrip <chris@walt.rip>
2022-11-08 15:33:41 -0800

    Print out more variables after build

--- infra/site/outputs.tf
+++ infra/site/outputs.tf
@@ -10,3 +10,29 @@ output "admin_api_key" {
   value = aws_api_gateway_api_key.short_urls_admin_api_key.value
   sensitive = true
 }
+
+output "cf_distribution_id" {
+  value       = aws_cloudfront_distribution.domain_cloudfront.id
+  description = "CloudFront distribution ID"
+}
+
+output "iam_publish_access_key" {
+  value       = aws_iam_access_key.publish.id
+  description = "Access Key ID for publish user"
+}
+
+output "iam_publish_secret_key" {
+  value       = aws_iam_access_key.publish.secret
+  description = "Secret access key for publish user"
+  sensitive = true
+}
+
+output "s3_bucket_url" {
+  value       = "s3://${aws_s3_bucket.site_bucket.id}?region=${aws_s3_bucket.site_bucket.region}"
+  description = "S3 site bucket URL"
+}
+
+output "s3_redirect_endpoint" {
+  value       = aws_s3_bucket_website_configuration.www_site_bucket.website_endpoint
+  description = "S3 www redirect endpoint"
+}
