2098c5b17faa159106a9cb098b3b22a4de995b0c
Chris Waltrip <chris@walt.rip>
2022-11-08 18:20:34 -0800

    Add FontAwesome in CSP

--- infra/site/lambda_functions/apply_security_headers/lambda_function.js
+++ infra/site/lambda_functions/apply_security_headers/lambda_function.js
@@ -4,7 +4,7 @@ exports.handler = (event, context, callback) => {
 	const headers = response.headers;
 
 	headers["strict-transport-security"] = [{key: "Strict-Transport-Security", value: "max-age=31536000; includeSubdomains; preload"}]; 
-	headers["content-security-policy"] = [{key: "Content-Security-Policy", value: "default-src 'none'; img-src 'self' www.google-analytics.com ; script-src 'self' 'unsafe-inline' www.google-analytics.com www.googletagmanager.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com; object-src 'none'"}]; 
+	headers["content-security-policy"] = [{key: "Content-Security-Policy", value: "default-src 'none'; connect-src *.fontawesome.com; img-src 'self'; script-src 'self' 'unsafe-inline' *.fontawesome.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com *.fontawesome.com; font-src 'self' fonts.googleapis.com fonts.gstatic.com *.fontawesome.com; object-src 'none'"}]; 
 	headers["x-content-type-options"] = [{key: "X-Content-Type-Options", value: "nosniff"}]; 
 	headers["x-frame-options"] = [{key: "X-Frame-Options", value: "DENY"}]; 
 	headers["x-xss-protection"] = [{key: "X-XSS-Protection", value: "1; mode=block"}]; 
